8ee9b7e084941678e471c8d913017a3004174310
mahdi <mahdimamashli1383@gmail.com>
2024-05-13 00:49:56 +0330

    "Update index.html and script.ts to connect username and password inputs to script"

--- frontend/script.ts
+++ frontend/script.ts
@@ -1,10 +1,6 @@
-const usernameInput = document.querySelector<HTMLInputElement>(
-  '[data-name="username"]'
-);
-const passwordInput = document.querySelector<HTMLInputElement>(
-  '[data-name="password"]'
-);
-
+const usernameInput = document.querySelector<HTMLInputElement>(".username");
+const passwordInput = document.querySelector<HTMLInputElement>(".password");
+const button = document.querySelector<HTMLButtonElement>(".btn_submit");
 async function postData(
   url: string,
   data: object
@@ -16,3 +12,19 @@ async function postData(
   });
   return await response.json();
 }
+
+button?.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const username = usernameInput?.value.trim();
+  const password = passwordInput?.value.trim();
+  if (!username || !password) {
+    alert("Please Enter Both Username or Password");
+    return;
+  }
+  const response = await postData("/signup", { username, password });
+  if (response.success) {
+    alert("User Created Successfully!");
+  } else {
+    alert(response.message);
+  }
+});
